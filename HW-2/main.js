9176945fb3943c57eb9f434e1c939af92d46688b
Lindsey Sewell <lindsey.sewell@umontana.edu>
2019-01-24 22:16:26 -0700

    Project2

---
+++ HW-2/main.js
@@ -0,0 +1,4 @@
+// I am writing something to the console
+console.log("Javascript is the best. My favorite restaurants are Ciao Mambo, Jade Garden, and Biaggis.");
+console.log("My favorite snacks are goldfish, pretzels, and fruit snacks.");
+console.log("My favorite fruits are peaches, strawberries and pears.");
