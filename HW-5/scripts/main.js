4ae49cae5df2f35aa627c4a26e14c7992a731f3e
Lindsey Sewell <lindsey.sewell@umontana.edu>
2019-02-13 20:01:23 -0700

    HW-5

---
+++ HW-5/scripts/main.js
@@ -0,0 +1,40 @@
+
+var imageTags = ["image1", "image2", "image3", "image4", "image5", "image6", "image7", "image8", "image9", "image10",];
+var blankImagePath = "images/blank1.png";
+var actualImages = new Array();
+function printBlanks()
+{
+    createRandomImageArray();
+    for(var i = 0; i < imageTags.length; i++)
+    {
+        document.getElementById(imageTags[i]).src= blankImagePath;
+    }
+}
+
+function createRandomImageArray()
+{
+
+    var actualImagePath = ["images/cat1.png", "images/cat2.png","images/cat3.png","images/cat4.png","images/cat5.png","images/cat6.png","images/cat7.png","images/cat1.png","images/cat8.png","images/cat9.png","images/cat10.png" ];
+
+    var count = [0,0,0,0,0,0,0,0,0,0];
+
+    while(actualImages.length < 10)
+    {
+      var randomNumber = Math.floor(Math.random() * actualImagePath.length);
+        if(count[randomNumber] < 5)
+        {
+            actualImages.push(actualImagePath[randomNumber]);
+            count[randomNumber] = count[randomNumber] + 1;
+        }
+    }
+
+
+}
+
+function flipImage(number)
+{
+    document.getElementById(imageTags[number]).src= actualImages[number];
+
+
+
+}
