556002495fa651e2d83d8879ae070b6664740013
Lindsey Sewell <lindsey.sewell@umontana.edu>
2019-04-30 15:44:30 -0600

    palettefix

--- FINAL/scripts/main.js
+++ FINAL/scripts/main.js
@@ -2,17 +2,18 @@ var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 var Prompts = ["You", "Your best friend", "your favorite book character", "your favorite movie character", "your favorite cartoon character", "your favorite superhero", "your favorite cartoon character", "your favorite Disney princess", "your favorite animal", "your favorite fictional couple", "your favorite TV character", "your pet", "a mermaid", "a witch", "a dragon", "a fairy", "a cat", "a princess", "a dog", "your original character", "your favorite singer", "an alien", "a new character of your design", "your favorite meal", "a sad drawing", "a happy drawing", "an angry character", "a soft-serve ice cream cone", "your favorite villain", "A new cover for your favorite book", "a fun typography of your own name", "a magical deer", "a pack of wolves", "a mountainous landscape", "a sunset", "a lake landscape", "the ocean", "an abstraction of color", "your favorite Disney prince", "an extremely angry bunny rabbit", "a dog in a swimming pool", "a mixture of different animals", "an alien creature of your design", "a ballerina", "a silly cartoon"];
 console.log (Prompts.length);
-var square = new Square(200, 0, 100, 100);
-var square2 = new Square(200, 100, 100, 100);
-var square3 = new Square(200, 200, 100, 100);
-var square4 = new Square(200, 300, 100, 100);
-var square5 = new Square(200, 400, 100, 100);
+var square = new Square(150, 250, 100, 100);
+var square2 = new Square(250, 250, 100, 100);
+var square3 = new Square(350, 250, 100, 100);
+var square4 = new Square(450, 250, 100, 100);
+var square5 = new Square(550, 250, 100, 100);
 var colorsArray = new Array();
 var randomNumber = 0;
 var randomNumber2 = 0;
 // var randomNumber = Math.floor (Math.random() * colorsArray.length);
 function displayPrompt(){
     ctx.font = "30px Arial";
+    ctx.fillStyle = "black";
 ctx.fillText(Prompts[randomNumber2], 10, 50); 
 }
 
